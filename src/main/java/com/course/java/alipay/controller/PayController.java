2082eadd7e300c6cb6351d38524524b2dba1a0c9
wangziyang <1974576606@qq.com>
2020-09-09 10:36:18 +0800

    spring boot集成支付宝demo

--- src/main/java/com/course/java/alipay/controller/PayController.java
+++ src/main/java/com/course/java/alipay/controller/PayController.java
@@ -25,6 +25,11 @@ public class PayController {
      * */
     @GetMapping(value = "/confirm" , produces = {"text/html;charset=UTF-8"})
     public Object pay (@RequestParam(required = false) PaymentBO bo) throws Exception {
+        //这个接口其实应该是post方式的，但是我这里图方便，直接以get方式访问，
+        //且返回格式是text/html，这样前端页面就能直接显示支付宝返回的html片段
+        //真实场景下由post方式请求，返回code、msg、data那种格式的标准结构，让前端拿到data里的
+        //html片段之后自行加载
+
         bo = new PaymentBO();
         return payService.pay(bo);
     }
